45ff1494f5f68a2cfc6c2ab63d098d42ecd29e07
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-18 00:09:54 -0700

    requirement

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import kickstarterData
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def hello():
+    return "Hello World!"
+
 @app.route('/kickstarter')
 def kickstarter():
     print("request.args", request.args)
+    if(request.args == None):
+        return "Invalid"
     result = kickstarterData.process(request.args)
     print("result", result)
-    return jsonify({"result": result["prediction"][0][1], "error": None})
+    response = jsonify({"result": result["prediction"][0][1], "error": None})
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ == '__main__':
     app.run()
